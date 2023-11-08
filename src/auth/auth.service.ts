ff1209947ffbb79ad6c5dde539dacbe44f3da705
이호준 <leecoder5359@gmail.com>
2023-11-08 23:48:37 +0900

    (feat): refresh 토큰 영속성 유지 로직 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,18 @@
 import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UserService } from 'src/user/user.service';
 import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RefreshToken } from './entity/refresh-token.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthService {
-    constructor(private userService: UserService, private jwtService: JwtService) {}
+    constructor(
+        private userService: UserService,
+        private jwtService: JwtService,
+        @InjectRepository(RefreshToken)
+        private refreshTokenRepository: Repository<RefreshToken>,
+    ) {}
 
     async validateUser(email: string, password: string): Promise<any> {
         return null;
@@ -24,8 +32,29 @@ export class AuthService {
         const isMatch = password == user.password;
         if (!isMatch) throw new UnauthorizedException();
 
+        const refreshToken = await this.generateRefreshToken(user.id);
+        await this.createRefreshTokenUsingUser(user.id, refreshToken);
+
         return {
-            acccessToken: this.jwtService.sign({ sub: user.id }),
+            accessToken: this.jwtService.sign({ sub: user.id }),
+            refreshToken,
         };
+    }
+
+    private generateRefreshToken(userId: string) {
+        const payload = { sub: userId, tokenType: 'refresh' };
+        return this.jwtService.sign(payload, { expiresIn: '1d' });
+    }
+
+    private async createRefreshTokenUsingUser(userId: string, refreshToken: string) {
+        let refreshTokenEntity = await this.refreshTokenRepository.findOneBy({ user: { id: userId } });
+
+        if (refreshTokenEntity) {
+            refreshTokenEntity.token = refreshToken;
+        } else {
+            refreshTokenEntity = this.refreshTokenRepository.create({ user: { id: userId }, token: refreshToken });
+        }
+
+        await this.refreshTokenRepository.save(refreshTokenEntity);
     }
 }
